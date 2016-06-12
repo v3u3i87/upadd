90bfb36bf79b3c3484028edc7597c02291cc4246
zmq <zhangmq@fangde.com>
2016-06-12 10:15:03 +0800

    add Log

---
+++ vendor/Upadd/Bin/Package/Log.php
@@ -0,0 +1,15 @@
+<?php
+namespace Upadd\Bin\Package;
+
+use Upadd\Bin\Factory;
+
+class Log extends Factory
+{
+
+    public static function getClassObj()
+    {
+        return 'Log';
+    }
+
+
+}
